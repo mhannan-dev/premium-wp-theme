5ba4c045d76fe31047a4b6cc4d57020265c61793
Muhammad Hannan <mdhannan.info@gmail.com>
2024-04-16 22:33:55 +0600

    Update

--- wp-content/themes/daily-job/flash_msg.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-            if (isset($_SESSION['flash_message'])) {
-                echo '<div class="flash-message">' . $_SESSION['flash_message'] . '</div>';
-                unset($_SESSION['flash_message']);
-            }
-          ?>
