b319f9227debadb9ec86b90c4d077b1b078ac5da
Muhammad Hannan <mdhannan.info@gmail.com>
2024-05-28 23:12:24 +0600

    Tags List and Comments Count

--- themes/beachtheme/index.php
+++ themes/beachtheme/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="container">
+			<div class="container home-blog-wrapper">
 				<?php 
 					if( have_posts() ):
 						while( have_posts() ): the_post();
--- themes/beachtheme/template-parts/content.php
+++ themes/beachtheme/template-parts/content.php
@@ -10,33 +10,32 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>'); ?>
-		<div class="entry-meta">
-			<?php echo sunset_posted_meta(); ?>
-		</div>
-	</header>
-	
-	<div class="entry-content">
-		
-		<?php if( has_post_thumbnail() ): ?>
-			
-			<div class="standard-featured"><?php the_post_thumbnail(); ?></div>
-			
-		<?php endif; ?>
-		
-		<div class="entry-excerpt">
-			<?php the_excerpt(); ?>
-		</div>
-		
-		<div class="button-container">
-			<a href="<?php the_permalink(); ?>" class="btn btn-default"><?php _e( 'Read More' ); ?></a>
-		</div>
-		
-	</div><!-- .entry-content -->
-	
-	<footer class="entry-footer">
-		<?php echo sunset_posted_footer(); ?>
-	</footer>
-	
+    <div class="entry-header">
+        <div class="title-container">
+            <?php the_title( '<h1 class="entry-title">', '</h1>'); ?>
+        </div>
+        <div class="meta-container">
+            <div class="entry-meta">
+                <?php echo sunset_posted_meta(); ?>
+            </div>
+        </div>
+    </div>
+    
+    <div class="entry-content">
+        <?php if( has_post_thumbnail() ): ?>
+            <div class="standard-featured"><?php the_post_thumbnail(); ?></div>
+        <?php endif; ?>
+        
+        <div class="entry-excerpt">
+            <?php the_excerpt(); ?>
+        </div>
+        
+        <div class="button-container">
+            <a href="<?php the_permalink(); ?>" class="btn btn-default read-more-btn"><?php _e( 'Read More' ); ?></a>
+        </div>
+    </div><!-- .entry-content -->
+    
+    <div class="entry-footer">
+        <?php echo sunset_posted_footer(); ?>
+    </div>
 </article>
